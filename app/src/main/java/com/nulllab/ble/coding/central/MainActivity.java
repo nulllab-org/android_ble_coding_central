7e9fa3166e4ebb440b0767ffa2346719af9242af
zomint <zomint01@163.com>
2024-04-16 10:52:06 +0800

    connecting by device id

--- app/src/main/java/com/nulllab/ble/coding/central/MainActivity.java
+++ app/src/main/java/com/nulllab/ble/coding/central/MainActivity.java
@@ -3,19 +3,21 @@ package com.nulllab.ble.coding.central;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.le.BluetoothLeScanner;
-import android.bluetooth.le.ScanCallback;
-import android.bluetooth.le.ScanResult;
-import android.bluetooth.le.ScanSettings;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -23,75 +25,35 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresPermission;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.app.ActivityCompat;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private final List<String> mBleAddressList = new LinkedList<>();
-    private final Set<String> mBleAddressSet = new HashSet<>();
-    private final LinkedHashMap<String, BluetoothDevice> mDiscoveredBleDevices = new LinkedHashMap<>();
-    ArrayAdapter<String> mDialogAdapter = null;
-    ArrayAdapter<String> mBleAddressSpinnerAdapter = null;
-    Spinner mBleAddressSpinner = null;
+    private final LinkedHashSet<String> mDeviceNames = new LinkedHashSet<>();
+    ArrayAdapter<String> mDeviceNameAdapter = null;
+    AutoCompleteTextView mDeviceNamesAutoCompleteTextView = null;
     SwitchCompat mConnectSwitch = null;
-    ScanCallback mScanCallback = new ScanCallback() {
-        @Override
-        @SuppressLint("MissingPermission")
-        @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
-        public void onScanResult(int callbackType, ScanResult result) {
-            super.onScanResult(callbackType, result);
-//            Log.d(TAG, "onScanResult: " + result);
-            if (result != null) {
-                BluetoothDevice device = result.getDevice();
-                if (device != null) {
-                    final String device_name = device.getName();
-                    if (device_name != null && device_name.startsWith(("codiplay_"))) {
-                        if (!mDiscoveredBleDevices.containsKey(device.getAddress())) {
-                            mDiscoveredBleDevices.put(device.getAddress(), device);
-                            mBleAddressList.add("Device Name: " + device_name + "\nMac Address: " + device.getAddress());
-                            mDialogAdapter.notifyDataSetChanged();
-                            mBleAddressSpinnerAdapter.notifyDataSetChanged();
-                        }
-                    }
-                }
-            }
-
-//            if (!mBleAddressSet.contains(address)) {
-//                    mBleAddressSet.add(address);
-//                    mBleAddressList.add(address);
-//                    mDialogAdapter.notifyDataSetChanged();
-//                    mBleAddressSpinnerAdapter.notifyDataSetChanged();
-//                }
-//            }
-        }
-
-        @Override
-        public void onBatchScanResults(List<ScanResult> results) {
-            super.onBatchScanResults(results);
-            Log.d(TAG, "onBatchScanResults: " + results);
-        }
-
-        @Override
-        public void onScanFailed(int errorCode) {
-            super.onScanFailed(errorCode);
-            Log.e(TAG, "onScanFailed: " + errorCode);
-        }
-    };
     private LogView mLogView;
     private TextView mStateTextView;
     private final BleCodingPeripheral mBleCodingPeripheral = new BleCodingPeripheral(this, new BleCodingPeripheral.Listener() {
@@ -109,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
                 case DISCONNECTED:
                     Toast.makeText(MainActivity.this, "device is disconnected", Toast.LENGTH_SHORT).show();
                     mConnectSwitch.setChecked(false);
+                    mDeviceNamesAutoCompleteTextView.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    mDeviceNamesAutoCompleteTextView.setTextColor(Color.GREEN);
                     break;
             }
         }
@@ -131,48 +95,151 @@ public class MainActivity extends AppCompatActivity {
         }
     });
 
+    void readDeviceNamesFromCache() {
+        File file = new File(getCacheDir(), "device_names.json");
+        if (file.length() == 0) {
+            return;
+        }
+        try {
+            FileInputStream inputStream = new FileInputStream(file);
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes);
+            inputStream.close();
+
+            JSONObject jsonObject = new JSONObject(new String(bytes));
+            JSONArray json_device_names = jsonObject.getJSONArray("device_names");
+            for (int i = 0; i < json_device_names.length(); i++) {
+                mDeviceNames.add(json_device_names.getString(i));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+
+    void addDeviceNameToCache(String deviceName) {
+        Log.d(TAG, "addDeviceNameToCache: " + deviceName);
+        File file = new File(getCacheDir(), "device_names.json");
+        mDeviceNames.remove(deviceName);
+        mDeviceNames.add(deviceName);
+        try {
+            JSONArray jsonArray = new JSONArray(mDeviceNames.toArray());
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("device_names", jsonArray);
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            bufferedWriter.write(jsonObject.toString(2));
+            bufferedWriter.close();
+        } catch (JSONException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        UpdateDeviceNameToTextView();
+    }
+
+    void UpdateDeviceNameToTextView() {
+        List<String> deviceNameList = Arrays.asList(mDeviceNames.toArray(new String[0]));
+        Collections.reverse(deviceNameList);
+
+        mDeviceNameAdapter.clear();
+        mDeviceNameAdapter.addAll(deviceNameList);
+        mDeviceNameAdapter.notifyDataSetChanged();
+        if (deviceNameList.size() > 0) {
+            mDeviceNamesAutoCompleteTextView.setText(deviceNameList.get(0));
+            mDeviceNamesAutoCompleteTextView.setTextColor(Color.GREEN);
+            mDeviceNamesAutoCompleteTextView.dismissDropDown();
+        }
+    }
+
     @Override
-    @SuppressLint("MissingPermission")
+    @SuppressLint({"MissingPermission", "ClickableViewAccessibility"})
     @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDialogAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, mBleAddressList);
-        mBleAddressSpinnerAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, mBleAddressList);
-//        mBleAddressSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mBleAddressSpinnerAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+
+        readDeviceNamesFromCache();
+        mDeviceNamesAutoCompleteTextView = findViewById(R.id.device_name);
+        mDeviceNameAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<>());
+        mDeviceNamesAutoCompleteTextView.setAdapter(mDeviceNameAdapter);
+        UpdateDeviceNameToTextView();
+        mDeviceNamesAutoCompleteTextView.setOnTouchListener((v, event) -> {
+            if (mDeviceNamesAutoCompleteTextView.getInputType() == InputType.TYPE_NULL) {
+                Toast.makeText(MainActivity.this, "please disconnected before modify device name", Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        });
+        mDeviceNamesAutoCompleteTextView.setInputType(InputType.TYPE_CLASS_NUMBER);
+        mDeviceNamesAutoCompleteTextView.setFilters(new InputFilter[]{new InputFilter.LengthFilter(6)});
+        mDeviceNamesAutoCompleteTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (mDeviceNamesAutoCompleteTextView.length() == 6) {
+                    mDeviceNamesAutoCompleteTextView.dismissDropDown();
+                    mDeviceNamesAutoCompleteTextView.setTextColor(Color.GREEN);
+                } else {
+                    mDeviceNamesAutoCompleteTextView.setTextColor(Color.RED);
+                }
+            }
+        });
+
         mConnectSwitch = findViewById(R.id.connect_switch);
         EditText codeEditText = findViewById(R.id.code_edit_text);
         mLogView = findViewById(R.id.log_text_view);
         mStateTextView = findViewById(R.id.state_text_view);
-        mBleAddressSpinner = findViewById(R.id.ble_address_spinner);
         mConnectSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             Log.d(TAG, "onCheckedChanged: " + isChecked);
+            if (!checkAndRequestScanPermissions()) {
+                enableBluetooth();
+                mConnectSwitch.setChecked(false);
+                return;
+            }
             if (!enableBluetooth()) {
                 mConnectSwitch.setChecked(false);
                 return;
             }
             if (isChecked) {
-                if (mBleAddressSpinner.getSelectedItem() == null) {
+                if (mDeviceNamesAutoCompleteTextView.getText() == null) {
+                    return;
+                }
+
+                String deviceIdStr = mDeviceNamesAutoCompleteTextView.getText().toString();
+                if (deviceIdStr.length() != 6) {
+                    Toast.makeText(MainActivity.this, "Please enter the 6-digit device ID correctly.", Toast.LENGTH_SHORT).show();
                     mConnectSwitch.setChecked(false);
-                    Toast.makeText(MainActivity.this, "please select a ble address", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (mDiscoveredBleDevices.isEmpty()) {
-                    return;
-                }
-//                mDiscoveredBleDevices.keySet().toArray()[mBleAddressSpinner.getSelectedItemPosition()].toString();
-//                mDiscoveredBleDevices.entrySet().toArray()[mBleAddressSpinner.getSelectedItemPosition()].toString();
-//                final String address = mBleAddressSpinner.getSelectedItem().toString();
-                final String address = mDiscoveredBleDevices.keySet().toArray()[mBleAddressSpinner.getSelectedItemPosition()].toString();
+                long deviceId = Long.parseLong(deviceIdStr);
+                deviceId += 0x100000000000L;
+                Log.d(TAG, "connect: " + Long.toString(deviceId, 16));
+                ByteBuffer macAddressBytes = ByteBuffer.allocate(Long.SIZE >> 3).putLong(deviceId);
+                String address = String.format("%02x:%02x:%02x:%02x:%02x:%02x",
+                        macAddressBytes.get(2), macAddressBytes.get(3), macAddressBytes.get(4), macAddressBytes.get(5), macAddressBytes.get(6), macAddressBytes.get(7));
+                Log.d(TAG, "address: " + address);
+
                 BleCodingPeripheral.ResultCode ret = mBleCodingPeripheral.connect(address);
+                addDeviceNameToCache(deviceIdStr);
                 if (BleCodingPeripheral.ResultCode.OK != ret) {
                     Log.e(TAG, "failed to connect " + address + ": " + ret);
                     Toast.makeText(MainActivity.this, "failed to connect " + address + ": " + ret, Toast.LENGTH_SHORT).show();
                     mConnectSwitch.setChecked(false);
+                    return;
+                } else {
+                    mDeviceNamesAutoCompleteTextView.setInputType(InputType.TYPE_NULL);
+                    mDeviceNamesAutoCompleteTextView.setTextColor(Color.GRAY);
                 }
             } else {
                 mBleCodingPeripheral.disconnect();
+                mDeviceNamesAutoCompleteTextView.setInputType(InputType.TYPE_CLASS_NUMBER);
+                mDeviceNamesAutoCompleteTextView.setTextColor(Color.GREEN);
             }
         });
 
@@ -191,42 +258,6 @@ public class MainActivity extends AppCompatActivity {
             if (ret != BleCodingPeripheral.ResultCode.OK) {
                 Toast.makeText(this, "failed to send data to stdin: " + ret, Toast.LENGTH_SHORT).show();
             }
-        });
-
-        mBleAddressSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.d(TAG, "onItemSelected: " + position);
-                mBleAddressSpinner.setSelection(position);
-                mConnectSwitch.setChecked(false);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Log.d(TAG, "onNothingSelected: ");
-                mConnectSwitch.setChecked(false);
-            }
-        });
-        mBleAddressSpinner.setAdapter(mBleAddressSpinnerAdapter);
-
-        findViewById(R.id.scan_button).setOnClickListener(v -> {
-            if (!checkAndRequestScanPermissions()) {
-                return;
-            }
-            if (!enableBluetooth()) {
-                return;
-            }
-            mBleCodingPeripheral.disconnect();
-            mBleAddressSpinnerAdapter.clear();
-            mBleAddressSpinnerAdapter.notifyDataSetChanged();
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            builder.setSingleChoiceItems(mDialogAdapter, 0, (dialog, which) -> ((Spinner) findViewById(R.id.ble_address_spinner)).setSelection(which))
-                    .setOnCancelListener(dialog -> BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner().stopScan(mScanCallback))
-                    .setTitle("scanning...")
-                    .setPositiveButton("OK", (dialog, which) -> BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner().stopScan(mScanCallback));
-            AlertDialog alertDialog = builder.create();
-            alertDialog.show();
-            startScan();
         });
 
         Spinner demoCodeSpinner = findViewById(R.id.demo_code);
@@ -302,7 +333,7 @@ public class MainActivity extends AppCompatActivity {
         String[] permissions = null;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            permissions = new String[]{Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT};
+            permissions = new String[]{Manifest.permission.BLUETOOTH_CONNECT};
         } else {
             permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
         }
@@ -348,30 +379,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return true;
-    }
-
-    @RequiresPermission(android.Manifest.permission.BLUETOOTH_SCAN)
-    private void startScan() {
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            Log.e(TAG, "connect: device doesn't support Bluetooth");
-            return;
-        }
-
-        if (!bluetoothAdapter.isEnabled()) {
-            Log.e(TAG, "connect: bluetooth is disabled");
-            return;
-        }
-        BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-
-        ScanSettings scanSettings = new ScanSettings.Builder().setMatchMode(ScanSettings.CALLBACK_TYPE_ALL_MATCHES).build();
-
-//        ScanFilter scanFilter = new ScanFilter.Builder().setServiceUuid(ParcelUuid.fromString("00000001-8c26-476f-89a7-a108033a69c7")).build();
-//        mBleAddressSet.clear();
-        mDiscoveredBleDevices.clear();
-        mBleAddressList.clear();
-        mDialogAdapter.notifyDataSetChanged();
-        bluetoothLeScanner.startScan(null, scanSettings, mScanCallback);
     }
 
     @Override
