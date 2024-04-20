09a9f063c9c05989b95cf3a90db2f2b3b581db8a
zomint <zomint01@163.com>
2024-04-20 15:38:04 +0800

    v1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.6"
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
